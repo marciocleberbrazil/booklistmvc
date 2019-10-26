2fca2e5c868d52f78c6247407aa364dd2f8f6a03
Marcio Barboza <marciocleberbrazil@gmail.com>
2019-10-26 15:21:53 +1100

    Create Generic Repository

---
+++ BookListMVC.Domain/Repositories/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookList.Domain.Repositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(int id);
+        Task<TEntity> GetAsync(int id);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+        // This method was not in the videos, but I thought it would be useful to add.
+        TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
