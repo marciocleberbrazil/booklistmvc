3556aa466a1b46e50b8581ed291799bf4d4df0f8
Marcio Barboza <marciocleberbrazil@gmail.com>
2019-10-26 16:19:29 +1100

    Create Books controller, add the Index action and create the list of book view

---
+++ BookListMVC/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookList.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+        private readonly IUnityOfWork _unityOfWork;
+
+        public BooksController(IUnityOfWork unityOfWork)
+        {
+            _unityOfWork = unityOfWork;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var books = await _unityOfWork.Books.GetAllAsync();
+
+            return View(books);
+        }
+    }
+}
