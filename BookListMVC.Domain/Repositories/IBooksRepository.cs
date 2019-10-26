4c7757240dc54dd1ff1161bd0cb596e905a62430
Marcio Barboza <marciocleberbrazil@gmail.com>
2019-10-26 15:26:07 +1100

    Create BooksRepository

---
+++ BookListMVC.Domain/Repositories/IBooksRepository.cs
@@ -0,0 +1,11 @@
+﻿using BookList.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookList.Domain.Repositories
+{
+    public interface IBooksRepository : IGenericRepository<Book>
+    {
+    }
+}
---
+++ BookListMVC.Infra.Data/Repositories/BooksRepository.cs
@@ -0,0 +1,22 @@
+﻿using BookList.Domain.Models;
+using BookList.Domain.Repositories;
+using BookList.Infra.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookList.Infra.Data.Repositories
+{
+    public class BooksRepository : GenericRepository<Book>, IBooksRepository
+    {
+        public BooksRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public ApplicationDbContext ApplicationDbContext
+        {
+            get { return Context as ApplicationDbContext; }
+        }
+    }
+}
