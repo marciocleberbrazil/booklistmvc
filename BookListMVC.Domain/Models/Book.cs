8f917becff77567eaa5c6406c24d2a13fc82c8db
Marcio Barboza <marciocleberbrazil@gmail.com>
2019-10-26 17:11:06 +1100

    Add create a new book

--- BookListMVC.Domain/Models/Book.cs
+++ BookListMVC.Domain/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace BookList.Domain.Models
@@ -7,6 +8,8 @@ namespace BookList.Domain.Models
     public class Book
     {
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
         public string Author { get; set; }
         public string ISBN { get; set; }
--- BookListMVC/Controllers/BooksController.cs
+++ BookListMVC/Controllers/BooksController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookList.Domain.Models;
 using BookList.Domain.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,6 +21,28 @@ namespace BookListMVC.Controllers
             var books = await _unityOfWork.Books.GetAllAsync();
 
             return View(books);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _unityOfWork.Books.Add(book);
+                var result = await _unityOfWork.CompleteAsync();
+
+                TempData["CreateSuccess"] = $"Book {book.Name} has been created";
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(book);
         }
     }
 }
