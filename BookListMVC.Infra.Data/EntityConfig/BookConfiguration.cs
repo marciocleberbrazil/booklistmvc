1d80c62db071856d85cb98844be807d6d027330c
Marcio Barboza <marciocleberbrazil@gmail.com>
2019-10-26 14:25:17 +1100

    Add Entity Book configuration

---
+++ BookListMVC.Infra.Data/EntityConfig/BookConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using BookListMVC.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookListMVC.Infra.Data.EntityConfig
+{
+    public class BookConfiguration : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.ToTable("Books");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .HasMaxLength(150)
+                .IsRequired();
+        }
+    }
+}
