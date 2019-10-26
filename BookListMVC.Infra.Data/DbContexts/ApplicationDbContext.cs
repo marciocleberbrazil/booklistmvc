96a7a7c35b626c78c1682b61b7560965b52e69b1
Marcio Barboza <marciocleberbrazil@gmail.com>
2019-10-26 14:32:13 +1100

    Create the main DbContext

---
+++ BookListMVC.Infra.Data/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BookListMVC.Domain.Models;
+using BookListMVC.Infra.Data.EntityConfig;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookListMVC.Infra.Data.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
+        }
+    }
+}
